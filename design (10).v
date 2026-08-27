d4624632a0971739eab50ba47b8701c3afdfb055
Rajesh S <rajeshsivaji28082006@gmail.com>
2026-08-27 23:47:02 +0530

    Add files via upload

---
+++ design (10).v
@@ -0,0 +1,42 @@
+module half_subtractor (
+    input A,
+    input B,
+    output Diff,
+    output Borrow
+);
+
+xor (Diff, A, B);
+and (Borrow, ~A, B);
+
+endmodule
+
+
+module full_subtractor (
+    input A,
+    input B,
+    input Bin,
+    output Diff,
+    output Bout
+);
+
+wire D1;
+wire B1;
+wire B2;
+
+half_subtractor HS1 (
+    .A(A),
+    .B(B),
+    .Diff(D1),
+    .Borrow(B1)
+);
+
+half_subtractor HS2 (
+    .A(D1),
+    .B(Bin),
+    .Diff(Diff),
+    .Borrow(B2)
+);
+
+or (Bout, B1, B2);
+
+endmodule
